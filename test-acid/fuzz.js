fd60441151dd1068e67a2386a3b735e563062d03
Cory Mawhorter <cory.mawhorter@gmail.com>
2015-09-27 13:18:49 -0700

    fuzz placeholder

---
+++ test-acid/fuzz.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var assert = require('assert');
+// TODO: implement ramdisk tests
+// var volume = require('./shared/volume.js');
+var Voyair = require('../index.js');
+
+
+// TODO: build fuzz testing on top of random ramdisk script
+
+describe('Voyair', function() {
+  it('should not error while being fuzz tested');
+});
